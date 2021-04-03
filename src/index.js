e4dc87f7c1475bfd1e2f7658b8d7fdd7f97c98b5
Thiago A. Marcelino <thiagodistend@hotmail.com>
2021-04-03 18:44:07 -0300

    Exercicios middlewares finalizados

--- src/index.js
+++ src/index.js
@@ -10,19 +10,80 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username} = request.headers;
+
+  const user = users.find(user => user.username === username);
+
+  if(!user){
+    return response.status(404).json({"error": "User not Exists"})
+  }
+
+  request.user = user;
+
+  return next();
+
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const {user} = request
+  
+  if(user.pro === false && user.todos.length >9){
+    return response.status(403).json("Operation not daned")
+  }
+
+  
+  console.log(user.todos.length)
+  
+  return next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const {username} = request.headers;
+  const {id} = request.params;
+
+const user = users.find(user => user.username === username)  
+  
+
+
+if(!user){
+  return response.status(404).json({"error": "User not exists"})
+}
+
+const todo = user.todos.find(todo => todo.id === id)
+
+const validId = validate(id);
+
+if(!validId){
+  return response.status(400).json({"error": "TODO ID invalid"})
+}
+
+if(!todo){
+  return response.status(404).json({"error": "TODO not exists"})
+}
+
+if(!user && !todo){
+  return response.status(400)
+}
+
+request.user = user;
+request.todo = todo;
+
+return next()
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const {id} = request.params;
+
+  const user = users.find(user => user.id === id);
+
+  if(!user){
+    return response.status(404).json("User not Exists");
+  }
+
+  request.user = user;
+
+  return next()
 }
 
 app.post('/users', (request, response) => {
